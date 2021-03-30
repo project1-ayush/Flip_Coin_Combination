6d98d8b7a99f085e126e5373efa7e877ff3414a3
project1-ayush <trivediayush491112@gmail.com>
2021-03-30 11:55:38 +0530

    Added: Singlet Combination In Dict

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,12 +1,40 @@
 #!/bin/bash -x
 
+declare -A Singlet_Comb
 Head=1
+i=0
+freqHead=0
+freqTail=0
+
+read -p "Enter Number Of Times You Want To Flip Coin" num
+echo $num
+
+n=$num
+
+while [[ $num -gt 0 ]]
+do
 
 randomFlip=$((RANDOM%2))
 
 if [ $randomFlip -eq $Head ]
 then
-		echo "Head"
+		Singlet_Comb[$i]="H"
+		((freqHead++))
 else
-		echo "Tail"
+		Singlet_Comb[$i]="T"
+		((freqTail++))
 fi
+
+((i++))
+((num--))
+done
+
+echo "${Singlet_Comb[@]}"
+
+hPercent=$(( ($freqHead * 100) / $n )) 
+echo $hPercent
+
+tPercent=$(( ($freqTail * 100) / $n )) 
+echo $tPercent
+
+
