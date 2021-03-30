460397c5ad43e200f8f30cfa3e3bc488f12c1f48
project1-ayush <trivediayush491112@gmail.com>
2021-03-30 13:00:43 +0530

    Added: Triplet Combination In Dict

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,19 +1,27 @@
 #!/bin/bash -x
 
-declare -A Doublet_Comb
+declare -A Triplet_Comb
 declare -A per_arr
 
-HH=0
-HT=1
-TH=2
-TT=3
+HHH=0
+HHT=1
+HTH=2
+HTT=3
+TTT=4
+TTH=5
+THT=6
+THH=7
 
 i=0
 
-freqHH=0
-freqHT=0
-freqTH=0
-freqTT=0
+freqHHH=0
+freqHHT=0
+freqHTH=0
+freqHTT=0
+freqTTT=0
+freqTTH=0
+freqTHT=0
+freqTHH=0
 
 read -p "Enter Number Of Times You Want To Flip Coin" num
 echo $num
@@ -23,44 +31,74 @@ n=$num
 while [[ $num -gt 0 ]]
 do
 
-randomFlip=$((RANDOM%4))
+randomFlip=$((RANDOM%8))
 
-if [ $randomFlip -eq $HH ]
-then
-		Doublet_Comb[$i]="HH"
-		((freqHH++))
-
-elif [ $randomFlip -eq $HT ]
-then
-		Doublet_Comb[$i]="HT"
-		((freqHT++))
-
-elif [ $randomFlip -eq $TH ]
-then
-		Doublet_Comb[$i]="TH"
-		((freqTH++))
+if [ $randomFlip -eq $HHH ]
+		then
+		Triplet_Comb[$i]="HHH"
+		((freqHHH++))
+elif [ $randomFlip -eq $HHT ]
+		then
+		Triplet_Comb[$i]="HHT"
+		((freqHHT++))
+elif [ $randomFlip -eq $HTH ]
+		then
+		Triplet_Comb[$i]="HTH"
+		((freqHTH++))
+elif [ $randomFlip -eq $HTT ]
+		then
+		Triplet_Comb[$i]="HTT"
+		((freqHTT++))
+elif [ $randomFlip -eq $TTT ]
+		then
+		Triplet_Comb[$i]="TTT"
+		((freqTTT++))
+elif [ $randomFlip -eq $TTH ]
+		then
+		Triplet_Comb[$i]="TTH"
+		((freqTTH++))
+elif [ $randomFlip -eq $THT ]
+		then
+		Triplet_Comb[$i]="THT"
+		((freqTHT++))
 else
-		Doublet_Comb[$i]="TT"
-		((freqTT++))
+		Triplet_Comb[$i]="THH"
+		((freqTHH++))
+
 fi
 
 ((i++))
 ((num--))
+
 done
 
-echo "${Doublet_Comb[@]}"
+echo "${Triplet_Comb[@]}"
 
-hhPercent=$(( ($freqHH * 100) / $n )) 
-per_arr[HH]=$hhPercent
+hhhPercent=$(( ($freqHHH * 100) / $n )) 
+per_arr[hhh]=$hhhPercent
 
-htPercent=$(( ($freqTT * 100) / $n )) 
-per_arr[HT]=$htPercent
+hhtPercent=$(( ($freqHHT * 100) / $n )) 
+per_arr[hht]=$hhtPercent
 
-thPercent=$(( ($freqTH * 100) / $n )) 
-per_arr[TH]=$thPercent
+hthPercent=$(( ($freqHTH * 100) / $n )) 
+per_arr[HTH]=$hthPercent
 
-ttPercent=$(( ($freqTT * 100) / $n )) 
-per_arr[TT]=$ttPercent
+httPercent=$(( ($freqHTT * 100) / $n )) 
+per_arr[HTT]=$httPercent
+
+
+tttPercent=$(( ($freqTTT * 100) / $n )) 
+per_arr[TTT]=$tttPercent
+
+tthPercent=$(( ($freqTTH * 100) / $n )) 
+per_arr[TTH]=$tthPercent
+
+thhPercent=$(( ($freqTHH * 100) / $n )) 
+per_arr[THH]=$thhPercent
+
+
+thtPercent=$(( ($freqTHT * 100) / $n )) 
+per_arr[THT]=$thtPercent
 
 
 echo "${!per_arr[@]}"
