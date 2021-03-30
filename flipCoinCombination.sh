07f690db336f17ac20aa17d6900650aad0726fa5
project1-ayush <trivediayush491112@gmail.com>
2021-03-30 12:24:05 +0530

    Added: Doublet Combination In Dict

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,19 @@
 #!/bin/bash -x
 
-declare -A Singlet_Comb
-Head=1
+declare -A Doublet_Comb
+declare -A per_arr
+
+HH=0
+HT=1
+TH=2
+TT=3
+
 i=0
-freqHead=0
-freqTail=0
+
+freqHH=0
+freqHT=0
+freqTH=0
+freqTT=0
 
 read -p "Enter Number Of Times You Want To Flip Coin" num
 echo $num
@@ -14,27 +23,46 @@ n=$num
 while [[ $num -gt 0 ]]
 do
 
-randomFlip=$((RANDOM%2))
+randomFlip=$((RANDOM%4))
 
-if [ $randomFlip -eq $Head ]
+if [ $randomFlip -eq $HH ]
 then
-		Singlet_Comb[$i]="H"
-		((freqHead++))
+		Doublet_Comb[$i]="HH"
+		((freqHH++))
+
+elif [ $randomFlip -eq $HT ]
+then
+		Doublet_Comb[$i]="HT"
+		((freqHT++))
+
+elif [ $randomFlip -eq $TH ]
+then
+		Doublet_Comb[$i]="TH"
+		((freqTH++))
 else
-		Singlet_Comb[$i]="T"
-		((freqTail++))
+		Doublet_Comb[$i]="TT"
+		((freqTT++))
 fi
 
 ((i++))
 ((num--))
 done
 
-echo "${Singlet_Comb[@]}"
+echo "${Doublet_Comb[@]}"
 
-hPercent=$(( ($freqHead * 100) / $n )) 
-echo $hPercent
+hhPercent=$(( ($freqHH * 100) / $n )) 
+per_arr[HH]=$hhPercent
 
-tPercent=$(( ($freqTail * 100) / $n )) 
-echo $tPercent
+htPercent=$(( ($freqTT * 100) / $n )) 
+per_arr[HT]=$htPercent
 
+thPercent=$(( ($freqTH * 100) / $n )) 
+per_arr[TH]=$thPercent
+
+ttPercent=$(( ($freqTT * 100) / $n )) 
+per_arr[TT]=$ttPercent
+
+
+echo "${!per_arr[@]}"
+echo "${per_arr[@]}"
 
